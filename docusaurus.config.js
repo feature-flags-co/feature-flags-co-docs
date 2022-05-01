8834508ef89066dc5d383af3d70b3c49101dd004
Comiscience <hu-beau@outlook.com>
2022-05-01 22:44:03 +0800

    updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl: 'https://github.com/feature-flags-co/feature-flags-co-docs',
         },
         blog: {
           showReadingTime: true,
